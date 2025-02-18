b58bc974360a7f7a9c68c27c4c8fb9c379184325
Daniel Johannsen <dj@danieljohannsen.de>
2025-02-18 22:22:25 +0100

    Add files via upload

---
+++ custom.el
@@ -0,0 +1,32 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(connection-local-criteria-alist
+   '(((:application eshell)
+      eshell-connection-default-profile)) t)
+ '(connection-local-profile-alist
+   '((eshell-connection-default-profile
+      (eshell-path-env-list))) t)
+ '(indent-tabs-mode nil)
+ '(package-selected-packages
+   '(js2-mode ledger-mode auctex auto-complete bbdb flycheck fountain-mode ivy markdown-mode org-journal org-roam org-transclusion web-mode writeroom-mode yaml-mode org htmlize wc-mode org-download apache-mode org-superstar highlight-indent-guides rjsx-mode gnuplot-mode font-lock-studio dired+ lua-mode fill-column-indicator ebib))
+ '(safe-local-variable-values
+   '((vc-prepare-patches-separately)
+     (diff-add-log-use-relative-names . t)
+     (vc-git-annotate-switches . "-w")
+     (ispell-dictionary . "english")
+     (org-ascii-text-width . 70)
+     (ispell-local-dictionary . english)))
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-indentation ((t (:foreground "firebrick"))))
+ '(whitespace-space ((t (:foreground "lightgray")))))
---
+++ init.el
@@ -0,0 +1,17 @@
+;; Variables:
+;; user-init-file: .emacs
+;; custom-file
+;; user-emacs-directory: .emacs.d
+
+
+;; keep customize settings in their own file
+(setq custom-file "~/.emacs.d/custom.el")
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+;; Use org mode for .emacs file:
+;; (require 'org)
+;; (org-babel-load-file (expand-file-name "emacs.org"
+;;                      user-emacs-directory))
+(load-file "~/.emacs.d/emacs.el")
+(put 'scroll-left 'disabled nil)
